362e0ae17695399f21c7ff5cb33f15156fd98ef9
doraeem <mehzebin.meem@gmail.com>
2025-09-18 03:13:53 +0600

    Add Header

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <header class="app-header">
+        <div class="left">
+            <h2 class="logo">MyNotes</h2>
+        </div>
+        <div class="right">
+            <input type="text" v-model="search" placeholder="Search notes..">
+            <button class="add-btn" @click="$emit('add-note')">+Add Note</button>
+        </div>
+    </header>
+</template>
+
+<script setup>
+import {ref,watch} from 'vue'
+const search = ref('')
+watch(search, (val)=>{
+    emit('update-search',val)
+})
+const emit = defineEmits(['add-note','update-search'])
+
+</script>
