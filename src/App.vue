2e9387275b289e3b2d8ef677d9d8e2bc635109b5
doraeem <mehzebin.meem@gmail.com>
2025-09-18 12:32:04 +0600

    Added Form

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,47 @@
 <template>
     <div id="app">
         <Header @add-note="addNote" @update-search="searchQuery = $event"></Header>
+        <div class="main-layout">
+         
+            <div class="editor">
+                <NoteCard
+                v-if="activeNote"
+                :note="activeNote"
+                @save-note="save-note"/>
+                <p v-else>Select or add a new note.</p>
 
+            </div>
 
+        </div>
     </div>
 </template>
 
 <script setup>
-import {ref} from 'vue'
+import {ref,computed} from 'vue'
 import Header from './components/Header.vue';
+import NoteCard from './components/NoteCard.vue';
+
+
+
+const notes = ref([])
+const activeNoteId = ref(null)
+
+function uid() { 
+    return 'n_' + Math.random().toString(36).substr(2, 9)
+     }
+     function addNote(){
+        const newNote = { id: uid(), title: '', content: '' }
+       notes.value.push(newNote)
+       activeNoteId.value = newNote.id
+}
+const activeNote = computed({
+  get() {
+    return notes.value.find(n => n.id === activeNoteId.value)
+  },
+  set(updatedNote) {
+    const index = notes.value.findIndex(n => n.id === updatedNote.id)
+    if (index !== -1) notes.value[index] = updatedNote
+  }
+})
+     
 </script>
---
+++ src/components/NoteCard.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="note-card">
+
+        <div class="note-card-header">
+            <span class="menu-dots" @click="menuOpen = !menuOpen">⋮</span>
+            <div v-if="menuOpen" class="menu-options">
+               <button @click="addImage">Add Image</button>
+               <button @click="makeSticky">Sticky Note</button>
+               <button @click="saveAsFile">Save as File</button>
+            </div>
+        </div>
+        <div class="note-card-body">
+            <input type="text"
+            v-model="note.title"
+            placeholder="Note Title"
+            class="note-title"
+            />
+            <textarea 
+            v-model="note.content"
+            placeholder="Write your note here.."
+            class="note-content">
+            </textarea>
+        </div>
+        <button class="save-btn" @click="saveNote">Save</button>
+        <button class="refresh-btn" @click="refreshNote">Refresh</button>
+
+    </div>
+    </template>
+
+<script setup>
+import {ref} from 'vue'
+
+const props= defineProps({
+    note:Object
+})
+const emit = defineEmits(['save-note'])
+
+const menuOpen = ref(false)
+
+function addImage() {
+  alert('Add Image feature placeholder')
+}
+
+function makeSticky() {
+  alert('Sticky Note feature placeholder')
+}
+function refreshNote(){
+    if(!props.note) return
+    props.note.title = ''
+    props.note.content = ''
+}
+
+function saveNote(){
+    emit('save-note',props.note)
+}
+</script>
